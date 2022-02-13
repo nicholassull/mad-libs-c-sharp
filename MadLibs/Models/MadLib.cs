20d8e200240e1db69116f46d88e7fcb0a966089e
Nick Sullivan <nicholassull@gmail.com>
2022-02-13 12:10:49 -0800

    add fields to MadLib.cs

--- MadLibs/Models/MadLib.cs
+++ MadLibs/Models/MadLib.cs
@@ -2,6 +2,16 @@ namespace MadLibs.Models
 {
   public class MadLib
   {
-    public string 
+    public string Adjective01 { get; set; }
+    public string Adjective02 { get; set; }
+    public string Adjective03 { get; set; }
+    public string Noun01 { get; set; }
+    public string Noun02 { get; set; }
+    public string Noun03 { get; set; }
+    public string Noun04 { get; set; }
+    public string Verb01 { get; set; }
+    public string Verb02 { get; set; }
+    public string Verb03 { get; set; }
+    public string TimeOfDay { get; set; }
   }
 }
